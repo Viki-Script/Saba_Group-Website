330407cc53608637c623bac3dac4b1adad71e024
Viki-Script <vigneshg0405@gmail.com>
2025-09-24 14:26:38 +0530

    initial

---
+++ script.js
@@ -0,0 +1,11 @@
+// button toggle
+
+  const buttons = document.querySelectorAll(".toggle-btn");
+
+  buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+      buttons.forEach(b => b.classList.remove("active"));
+      btn.classList.add("active");
+    });
+  });
+
